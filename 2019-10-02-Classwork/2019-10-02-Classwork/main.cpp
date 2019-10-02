34e1f4623cc8cac6caebbe8630bfeab6b867d8be
tacscandice <55443460+tacscandice@users.noreply.github.com>
2019-10-02 08:37:48 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/main.cpp
@@ -0,0 +1,26 @@
+// Name: Candice Duncan
+// Intro to Computer Science
+// Mr. Williams
+// Period 1
+// 2019-10-02-Classwork
+
+#include <iostream>
+int main() 
+{
+	char letterA = 'A';
+	char letterB = 'B';
+	char letterC = 'C';
+	char letterD = 'D';
+
+	short life = 1;
+	int periods = 10;
+	int work = 9;
+
+	bool isHot = true;
+	bool isItFire = false;
+
+	float time = 6;
+	float school = 7;
+	double life = 8;
+
+}
