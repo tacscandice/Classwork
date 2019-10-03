1491d2e9a8c5c4930dc908ca7644d3feea26ab89
tacscandice <55443460+tacscandice@users.noreply.github.com>
2019-10-03 08:37:56 -0400

    Add files via upload

---
+++ 2019-10-03-Classwork/2019-10-03-Classwork/main.cpp
@@ -0,0 +1,17 @@
+// Name: Candice Duncan
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter your favorite number: ";
+	int favoriteNumber;
+	std::cin >> favoriteNumber;
+	std::cout << "Cool, " << favoriteNumber << " is my favorite number too!" << std::endl;
+
+}
+
